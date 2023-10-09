d63ff7c46bba3204bbeaff4cd3b0aee1c0e66efd
Natalia <PalejNatalia@gmail.com>
2023-10-09 15:51:10 +0100

    Created empty classes and JUnits - failing as expected

---
+++ src/agile_project/NewsagentUserTest.java
@@ -0,0 +1,39 @@
+package agile_project;
+
+import junit.framework.TestCase;
+
+public class NewsagentUserTest extends TestCase {
+
+	public void testNewsagentUser01() {
+		fail("Not yet implemented");
+	}
+
+	public void testCreateNewCustomer01() {
+		fail("Not yet implemented");
+	}
+
+	public void testUpdateNewCustomer01() {
+		fail("Not yet implemented");
+	}
+
+	public void testDeactivateCustomer01() {
+		fail("Not yet implemented");
+	}
+
+	public void testReadCustomerDetails01() {
+		fail("Not yet implemented");
+	}
+
+	public void testPrintCustomerDetails01() {
+		fail("Not yet implemented");
+	}
+
+	public void testGenerateInvoice01() {
+		fail("Not yet implemented");
+	}
+
+	public void testGenerateDeliveryDocketReport01() {
+		fail("Not yet implemented");
+	}
+
+}

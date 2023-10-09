deae7f9f5b6dc886d33da8ef2cbf970f94b2849a
Natalia <PalejNatalia@gmail.com>
2023-10-09 15:16:15 +0100

    Created empty User and UserTest class, JUnit fails as expected

---
+++ src/agile_project/UserTest.java
@@ -0,0 +1,31 @@
+package agile_project;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+
+	public void testUser01() {
+		fail("Not yet implemented");
+	}
+
+	public void testCreateUserAccount01() {
+		fail("Not yet implemented");
+	}
+
+	public void testUpdateUserAccount01() {
+		fail("Not yet implemented");
+	}
+
+	public void testDeleteUserAccount01() {
+		fail("Not yet implemented");
+	}
+
+	public void testReadUserAccount01() {
+		fail("Not yet implemented");
+	}
+
+	public void testPrintUserAccount01() {
+		fail("Not yet implemented");
+	}
+
+}

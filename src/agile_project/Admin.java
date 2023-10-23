68b4cc8751a5a2ba1cfded6891d6e0f8fc6d1105
Natalia <PalejNatalia@gmail.com>
2023-10-23 14:59:23 +0100

    Working on User and Admin class

--- src/agile_project/Admin.java
+++ src/agile_project/Admin.java
@@ -1,15 +1,50 @@
 package agile_project;
 
-public class Admin {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Admin extends DatabaseConnector {
+	
+	// Create connection to the database
+	Connection connection = getConnection();
 
 	private int adminID;
 	private String username, password, role;
 	
-	public AdminUser(int adminID, String username, String password, String role) throws NataliaException {
+	public Admin(int adminID, String username, String password, String role) throws NataliaException {
 		throw new NataliaException("Constructor not yet implemented");
+	}
+	
+	public void createUser(String username, String password, String role) throws NataliaException, SQLException {
+		User user = new User(username, password, role);
+		saveUserToDatabase(user);
+	}
+	
+	public void deleteUser(int id) {
+		
+	}
+	
+	public String readUser(int id) {
+		return null;
+	}
+	
+	public void updateUser() {
+		
 	}
 	
 	public void givePermission() throws NataliaException {
 		throw new NataliaException("Method not yet implemented");
+	}
+	
+	public void saveUserToDatabase(User user) throws NataliaException, SQLException {
+		String sql = "INSERT INTO newsagentdb VALUES (?, ?, ?)";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, user.getUsername());
+		stmt.setString(2, user.getPassword());
+		stmt.setString(3, user.getRole());
+		stmt.execute(sql);
+		stmt.executeUpdate();
+		System.out.println("User : " + user + " was successfully created!");
 	}
 }
--- src/agile_project/User.java
+++ src/agile_project/User.java
@@ -1,5 +1,50 @@
 package agile_project;
 
 public class User {
+	private String username, password, role;
+	
+	// User constructor 
+	public User(String username, String password, String role) throws NataliaException {
+		this.username = username;
+	    this.password = password;
+	    this.role = role;
+	}
+	
+	/** 
+	 * User methods
+	 * @param username between 1-10 characters
+	 * @param password between 6-10 characters 
+	 * @param role : admin/newsagent/driver
+	 */
+	
+	public static User createUserAccount(String username, String password, String role) throws NataliaException {
+		return new User(username, password, role);
+	}
+	
+	/**
+	 * Getters and setters
+	 */
+	public String getUsername() {
+		return username;
+	}
 
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 }

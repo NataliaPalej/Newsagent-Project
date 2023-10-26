0741b1243660995ba3fd7ba21d2aba139f168f3d
Natalia <PalejNatalia@gmail.com>
2023-10-26 19:36:19 +0100

    Working on Admin class

--- src/agile_project/Admin.java
+++ src/agile_project/Admin.java
@@ -17,20 +17,22 @@ public class Admin extends DatabaseConnector {
 	}
 	
 	public void createUser(String username, String password, String role) throws NataliaException, SQLException {
+		connection = DatabaseConnector.getConnection();
 		User user = new User(username, password, role);
 		saveUserToDatabase(user);
 	}
 	
 	public void deleteUser(int id) {
-		
+		connection = DatabaseConnector.getConnection();
 	}
 	
 	public String readUser(int id) {
+		connection = DatabaseConnector.getConnection();
 		return null;
 	}
 	
 	public void updateUser() {
-		
+		connection = DatabaseConnector.getConnection();
 	}
 	
 	public void givePermission() throws NataliaException {
@@ -38,13 +40,82 @@ public class Admin extends DatabaseConnector {
 	}
 	
 	public void saveUserToDatabase(User user) throws NataliaException, SQLException {
-		String sql = "INSERT INTO newsagentdb VALUES (?, ?, ?)";
-		PreparedStatement stmt = connection.prepareStatement(sql);
+		String query = "INSERT INTO userdetails VALUES (?, ?, ?)";
+		PreparedStatement stmt = connection.prepareStatement(query);
 		stmt.setString(1, user.getUsername());
 		stmt.setString(2, user.getPassword());
 		stmt.setString(3, user.getRole());
-		stmt.execute(sql);
+		stmt.execute(query);
 		stmt.executeUpdate();
 		System.out.println("User : " + user + " was successfully created!");
+	}
+
+	/** 
+	 * Validation methods
+	 * @param username between 1-10 characters
+	 * @param password between 6-10 characters + at least one number&upper case
+	 * @param role: admin/newsagent/driver
+	 */
+	public boolean isValidUsername(String username) {
+		return false;
+	}
+	
+	public boolean isValidPassword(String password) {
+		if (password.length() < 6 || password.length() > 10) {
+			return false;
+		} // Check if password contains at least one digit
+		if (!password.matches(".*\\d.*")) {
+			return false;
+		} // Check if the password contains at least one uppercase letter
+		if (!password.matches(".*[A-Z].*")) {
+	        return false;
+	    }
+		return true;
+	}
+	
+	public boolean isValidRole(String role) {
+		if (role == "admin" || role == "driver" || role == "newsagent") {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Setters & Getters
+	 * @return
+	 */
+	
+	public int getAdminID() {
+		return adminID;
+	}
+
+	public void setAdminID(int adminID) {
+		this.adminID = adminID;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
 	}
 }

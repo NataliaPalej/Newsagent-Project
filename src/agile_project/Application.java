64950de275a402427933b65e3691a78367a046d0
Natalia <PalejNatalia@gmail.com>
2023-10-27 18:13:26 +0100

    Started working on the CommandLine for Application

--- src/agile_project/Application.java
+++ src/agile_project/Application.java
@@ -3,15 +3,25 @@
  */
 package agile_project;
 
-import java.sql.*;
 import java.util.Scanner;
+import agile_project.FINISHED.DatabaseConnector;
 
-public class Newsagent extends DatabaseConnector {
+public class Application extends DatabaseConnector {
 	
-	private static Connection connection = DatabaseConnector.getConnection();
 	private static Scanner in = new Scanner(System.in);
 	
 	public static void main(String[] args) {
+		
+		System.out.println(" ---------------------------------------");
+        System.out.println("| Welcome to the Newsagent Application! |");
+        System.out.println("|---------------------------------------|");
+        
+        System.out.println("|              Login                    |");
+        System.out.print("|        Enter username:                |");
+        String username = in.next();
+        System.out.print("|        Enter password:                |");
+        String password = in.next();
+        System.out.println(" ---------------------------------------\n");
 		
 	}
 }

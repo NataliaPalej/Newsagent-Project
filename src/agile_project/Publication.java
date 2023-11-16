0d9b8f371c588d6aee8b5ac16f1e165aa358f21b
47-r0nan <ronan.harris1@gmail.com>
2023-11-16 09:52:39 +0000

    updated publication.java#

--- src/agile_project/Publication.java
+++ src/agile_project/Publication.java
@@ -7,19 +7,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Publication {
-	
+
 	private int id;
 	private String title;
 	private int issueNo;
 	private String author;
 	private double price;
 	private int stock;
-	
+
 	public Publication() throws RonanException {
 		// TODO Auto-generated constructor stub
-		
+
 	}
-	
+
 	public Publication(String title, int issueNo, String author, double price, int stock) throws RonanException {
 		// TODO Auto-generated constructor stub
 		this.title = title;
@@ -28,41 +28,41 @@ public class Publication {
 		this.price = price;
 		this.stock = stock;
 	}
-	
+
 	public void createNewPublication() throws RonanException {
 		throw new RonanException("createNewPublication() not implemented");
 	}
-	
+
 	public void updatePublication() throws RonanException {
 		throw new RonanException("updatePublication() not implemented");
 	}
-	
+
 	public void deletePublication() throws RonanException {
 		throw new RonanException("deletePublication() not implemented");
 	}
 
 	public Publication getPublicationById(int id) throws RonanException {
-//		throw new RonanException("getPublicationById() not implemented");
+		//		throw new RonanException("getPublicationById() not implemented");
 		System.out.println("* ---------------------------- *");
-    	System.out.println("|  Print Publication Details   |");
-    	System.out.println("* ---------------------------- *");
-		
+		System.out.println("|  Print Publication Details   |");
+		System.out.println("* ---------------------------- *");
+
 		Connection connection = null;
-	    PreparedStatement preparedStatement = null;
-	    ResultSet resultSet = null;
-	    
-	    Publication publicationObj = new Publication();
-		
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		Publication publicationObj = new Publication();
+
 		try {
 			connection = DatabaseConnector.getConnection();
-			
+
 			String query = "SELECT * FROM publications WHERE publicationID = ?";
 			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setInt(1, id);
 			resultSet = preparedStatement.executeQuery();
-			
+
 			if (resultSet.next()) {
-				// Retrieve customer details from the result set
+				// Retrieve publication details from the result set
 				publicationObj.setId(id);
 				publicationObj.setTitle(resultSet.getString("title"));
 				publicationObj.setIssueNo(resultSet.getInt("issueNo"));
@@ -70,48 +70,48 @@ public class Publication {
 				publicationObj.setPrice(resultSet.getDouble("price"));
 				publicationObj.setStock(resultSet.getInt("stock"));
 
-	            
-	            System.out.println("Publication ID: " + publicationObj.getId() + "\nTitle: " + publicationObj.getTitle() + "\nIssue No.: " 
-	            + publicationObj.getIssueNo() + "\n" + "Author: " + publicationObj.getAuthor() + "\nPrice: " + publicationObj.getPrice()+ "\nStock: " + publicationObj.getStock());
+
+				System.out.println("Publication ID: " + publicationObj.getId() + "\nTitle: " + publicationObj.getTitle() + "\nIssue No.: " 
+						+ publicationObj.getIssueNo() + "\n" + "Author: " + publicationObj.getAuthor() + "\nPrice: " + publicationObj.getPrice()+ "\nStock: " + publicationObj.getStock());
 			} else {
 				throw new RonanException("Publication with ID(" + publicationObj.getId() + ") NOT found.");
 			}
 		} catch (SQLException error) {
 			throw new RonanException("Database error.\n" + error.getMessage());
 		} finally {
-	        // Close the database resources
-	        try {
-	            if (resultSet != null) resultSet.close();
-	            if (preparedStatement != null) preparedStatement.close();
-	            if (connection != null) connection.close();
-	        } catch (SQLException e) {
-	            // Handle resource closing exceptions
-	            throw new RonanException("Error while closing database resources.\n" + e.getMessage());
-	        }
+			// Close the database resources
+			try {
+				if (resultSet != null) resultSet.close();
+				if (preparedStatement != null) preparedStatement.close();
+				if (connection != null) connection.close();
+			} catch (SQLException e) {
+				// Handle resource closing exceptions
+				throw new RonanException("Error while closing database resources.\n" + e.getMessage());
+			}
 		}
 		return publicationObj;
 	}
-	
-public String getAllCustomers() throws RonanException {
-		
+
+	public String getAllPublications() throws RonanException {
+
 		System.out.println("* ----------------------------- *");
-    	System.out.println("|  Print All Customers Details  |");
-    	System.out.println("* ----------------------------- *");
-		
+		System.out.println("|  Print All Publications Details  |");
+		System.out.println("* ----------------------------- *");
+
 		Connection connection = null;
-	    PreparedStatement preparedStatement = null;
-	    ResultSet resultSet = null;
-	    
-	    try {
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
 			connection = DatabaseConnector.getConnection();
-			
+
 			String query = "SELECT * FROM publications ORDER BY publicationID ASC";
 			preparedStatement = connection.prepareStatement(query);
 			resultSet = preparedStatement.executeQuery();
-			
+
 			boolean publicationList = false;
 			String allPublicationsDetails = "";
-			
+
 			while (resultSet.next()) {
 				publicationList = true;
 				String publicationID = resultSet.getString("publicationID");
@@ -120,55 +120,57 @@ public String getAllCustomers() throws RonanException {
 				String author = resultSet.getString("author");
 				double price = resultSet.getDouble("price");
 				int stock = resultSet.getInt("stock");
-	            
-	            String formattedCustID = String.format("%-8s", custID);
-	            String formattedFirstName = String.format("%-15s", firstName);
-	            String formattedLastName = String.format("%-15s", lastName);
-	            String formattedAddress = String.format("%-20s", address);
-	            String formattedPhoneNo = String.format("%-15s", phoneNo);
-	            
-	            System.out.println("Customer ID: " + formattedCustID +
-	                    "First Name: " + formattedFirstName +
-	                    "Last Name: " + formattedLastName +
-	                    "Address: " + formattedAddress +
-	                    "Phone Number: " + formattedPhoneNo);
+
+				String formattedPublicationID = String.format("%-8s", publicationID);
+				String formattedTitle = String.format("%-15s", title);
+				String formattedIssueNo = String.format("%-15s", issueNo);
+				String formattedAuthor = String.format("%-20s", author);
+				String formattedPrice = String.format("%-15s", price);
+				String formattedStock = String.format("%-15s", stock);
+
+				System.out.println("Publication ID: " + formattedPublicationID +
+						"Title: " + formattedTitle +
+						"Issue No.: " + formattedIssueNo +
+						"Author: " + formattedAuthor +
+						"Price: " + formattedPrice +
+						"Stock: " + formattedStock);
 			}
-			if (!customerList) {
-	            throw new RonanException("Customer database is empty or not found.");
-	        }
-			return allCustomersDetails.toString();
+			if (!publicationList) {
+				throw new RonanException("Publication database is empty or not found.");
+			}
+			return allPublicationsDetails.toString();
 		} catch (SQLException error) {
 			throw new RonanException("Database error.\n" + error.getMessage());
 		} finally {
-	        try {
-	            if (resultSet != null) resultSet.close();
-	            if (preparedStatement != null) preparedStatement.close();
-	            if (connection != null) connection.close();
-	        } catch (SQLException e) {
-	            throw new RonanException("Error while closing database resources.\n" + e.getMessage());
-	        }
+			try {
+				if (resultSet != null) resultSet.close();
+				if (preparedStatement != null) preparedStatement.close();
+				if (connection != null) connection.close();
+			} catch (SQLException e) {
+				throw new RonanException("Error while closing database resources.\n" + e.getMessage());
+			}
 		}
 	}
-	
+
 	public void updateStock() throws RonanException {
 		throw new RonanException("updateStock() not implemented");
 	}
-	
+
 	public boolean isValidInt(int x) throws RonanException {
 		if (x >= 0) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public boolean isValidPrice(double price) throws RonanException {		
 		if (price >= 0.01 & price <= 999.99 & !(BigDecimal.valueOf(price).scale() > 2)) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	public boolean isValidString(String string) throws RonanException {
 		if (string != null) {
 			if (string.length() <= 50 & !(string.isBlank())) {
@@ -177,8 +179,8 @@ public String getAllCustomers() throws RonanException {
 		}
 		return false;
 	}
-	
-	
+
+
 	public int getId() {
 		return id;
 	}
@@ -239,6 +241,6 @@ public String getAllCustomers() throws RonanException {
 	}
 
 
-	
+
 
 }

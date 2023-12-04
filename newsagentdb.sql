983f25839fa1286d352117348cd66ee6327c1f2e
Ivan Lapickij <116425938+IvanLapickij@users.noreply.github.com>
2023-12-04 15:45:10 +0000

    works

--- newsagentdb.sql
+++ newsagentdb.sql
@@ -89,30 +89,6 @@ CREATE TABLE IF NOT EXISTS customerdetails (
     areaCode int NOT NULL
 );
 
--- INSERT INTO customers 
-INSERT INTO customerdetails (firstName, lastName, address, phoneNo, areaCode) VALUES
-('John', 'Doe', '123 Main St , Carlow', '555-986-7254', 5),
-('Alice', 'Smith', '456 Elm St, Dublin', '555-986-6654', 4),
-('Robert', 'Johnson', '789 Oak St, Dublin', '555-555-6555', 3),
-('Sara', 'Williams', '1122 Elm St, Dublin', '555-776-7777', 2),
-('Michael', 'Brown', '88 Oak St, Kildare', '555-999-6999', 1),
-('Emily', 'Miller', '321 Pine St,Kildare', '555-333-4644', 12),
-('Daniel', 'Clark', '789 Snow St, Kilkenny', '555-555-6555', 11),
-('Emanuel', 'Garcia', '111 Three St, Laois', '555-767-7777', 10),
-('Ciara', 'Taylor', '222 River St, Longford', '555-969-9999', 9),
-('Mark', 'Johnson',  '789 Oak St, Carlow', '555-444-6555', 8),
-('Emma', 'Davis',  '456 Pine St, Dublin', '555-333-6666', 7),
-('Christopher', 'Lee', '123 Maple St, Kildare', '555-622-2777', 6),
-('Olivia', 'Moore',  'Birch St, Kilkenny', '555-111-6888', 5),
-('James', 'White', 'Laois', '555-777-6000', 4),
-('Sophia', 'Hall',  'Redwood St, Longford', '555-888-6999', 1),
-('Liam', 'Jones',  'Willow St, Louth', '555-999-1611', 2),
-('Ava', 'Taylor', 'Oak St, Meath', '555-222-3363', 3),
-('Noah', 'Brown',  'Pine St, Offaly', '555-444-5565', 12),
-('Isabella', 'Wilson',  'Elm St, Westmeath', '555-666-7677', 11),
-('Logan', 'Anderson', 'Maple St, Wexford', '555-888-9996', 10);
-
-
 -- Create the 'orders' table
 DROP TABLE IF EXISTS orders;
 select * from orders;
@@ -153,37 +129,19 @@ INSERT INTO orders (dateCreated, custID, orderType, publicationID) VALUES
 
 -- INSERT INTO ORDERS
 INSERT INTO orders (dateCreated, custID, orderType, publicationID) VALUES 
-('2023-10-10', 1, 'daily', 20),
-('2023-10-20', 2, 'weekly', 19),
-('2023-10-21', 3, 'monthly', 18),
-('2023-10-22', 4, 'daily', 17),
-('2023-10-23', 5, 'weekly', 16),
-('2023-10-24', 6, 'monthly', 15),
-('2023-10-25', 7, 'daily', 14),
-('2023-10-26', 8, 'weekly', 13), 
-('2023-10-27', 9, 'monthly', 12), 
-('2023-10-28', 10, 'daily', 11),
-('2023-11-13', 11, 'weekly', 10),
-('2023-11-13', 12, 'weekly', 9),
-('2023-11-13', 13, 'monthly', 8),
-('2023-11-13', 14, 'daily', 7),
-('2023-11-13', 15, 'weekly', 6),
-('2023-11-13', 16, 'monthly', 5),
-('2023-11-13', 17, 'daily', 4),
-('2023-11-13', 18, 'weekly', 3), 
-('2023-11-13', 19, 'monthly', 2), 
-('2023-11-13', 20, 'daily', 1),
-('2023-11-13', 21, 'weekly', 20),	
-('2023-11-13', 22, 'monthly', 19),
-('2023-11-13', 23, 'daily', 18),
-('2023-11-13', 24, 'weekly', 17),
-('2023-11-13', 25, 'monthly', 16),
-('2023-11-13', 26, 'daily', 15),
-('2023-11-13', 27, 'weekly', 14), 
-('2023-11-13', 28, 'monthly', 13), 
-('2023-11-13', 29, 'daily', 12),
-('2023-11-13', 2, 'weekly', 11),
-('2023-11-13', 1, 'monthly', 10);
+('2023-10-10', 1, 'daily', 1),
+('2023-10-20', 2, 'weekly', 1),
+('2023-10-21', 3, 'monthly', 1),
+('2023-10-22', 4, 'daily', 2),
+('2023-10-23', 5, 'weekly', 2),
+('2023-10-24', 6, 'monthly', 2),
+('2023-10-25', 7, 'daily', 2),
+('2023-10-26', 8, 'weekly', 3), 
+('2023-10-27', 9, 'monthly', 3), 
+('2023-10-28', 10, 'daily', 3),
+('2023-11-13', 11, 'weekly', 4),
+('2023-11-13', 2, 'weekly', 1),
+('2023-11-13', 1, 'monthly', 1);
 
 -- Temporary table to generate dates up to 06/12/2023
 CREATE TEMPORARY TABLE temp_dates AS (
